bbf5a790cc09df2aba7e8d96c5e996b790f73b68
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-08-20 09:55:51 +0300

    loginApp_2_fix_bags

--- LoginApp_2/ViewControllers/LoginViewController.swift
+++ LoginApp_2/ViewControllers/LoginViewController.swift
@@ -12,44 +12,56 @@ final class LoginViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passWordTF: UITextField!
     
-    private let user = "1"
+    private let user = "Pavel"
     private let password = "123"
-                
+    
+    //MARK: - Override functions
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        welcomeVC.welcomeUserText = userNameTF.text ?? ""
+        //привязка опционала
+//        welcomeVC.welcomeUserText = userNameTF.text ?? ""
+        welcomeVC.welcomeUserText = user
     }
-    
+   
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
     
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        identifier == "1"
-    }
+//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//        identifier == user
+//    } // - получается этот метод не нужен? 
     
+    //MARK: - IBAction functions
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         userNameTF.text = ""
         passWordTF.text = ""
     }
     
-    @IBAction func logInButtonPressed() {
+    @IBAction func logInButtonPressed() { //IBAct срабатывает когда нажимаешь кнопку
         guard userNameTF.text == user && passWordTF.text == password else {
             showAlert(with: "Invalid login or password", and: "Please, enter correct login and password")
             return
-            
         }
+//        shouldPerformSegue(withIdentifier: <#T##String#>, sender: nil) - заменил на performSegue
+        performSegue(withIdentifier: "openWelcomeVC", sender: nil) //что этот метод делает? - инициирует вызов метода prepare(for Segue
     }
     
     @IBAction func forgotUNPressed() {
-        showAlert(with: "OOPS!", and: "Your name is \(user)")
+        showAlert(with: "OOPS!", and: "Your name is \(user)😉")
     }
         
     @IBAction func forgotPWPressed() {
-        showAlert(with: "OOPS!", and: "Your password is \(password)")
+        showAlert(with: "OOPS!", and: "Your password is \(password)😉")
     }
 }
+
+//@IBAction func forgotRegisterData(_ sender: UIButton) { // привязать к двум кнопкам
+//    sender.tag == 0
+//    ? showAlert(with: "OOPS!", and: "Your name is \(user)") // это ветка если
+//    : showAlert(with: "OOPS!", and: "Your password is \(password)") // это ветка то
+//}
+
 //MARK: - UIAlertController
 extension LoginViewController {
     private func showAlert(with logInButton: String, and message: String) {
